9be37f1599f3be26743eed2dd7ddd8b6a7f973ca
Alvin <al.charity@gmail.com>
2021-07-16 00:04:33 -0400

    router -> hashrouter, prep to consolidate

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import * as React from "react";
 import { Component } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as HashRouter,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 // import trails_list from "./trails_list"
 import "./index.css";
 
 const trails_list = {
   "wheaton regional": {
     address: "2000 Shorefield Road, Wheaton, Maryland 20902",
-    phone: "3014952595",
+    phone: "(301) 495-2595",
     website:
       "https://www.montgomeryparks.org/parks-and-trails/wheaton-regional-park/",
     trails: {
@@ -66,10 +71,13 @@ const trails_list = {
 
 function Navigation() {
   return (
-    <Router>
-      <div>
-        <nav style={{ height: "10vh" }}>
-          <div>
+    <div>
+      <HashRouter>
+        <div>
+          <nav style={{ height: "10vh" }}>
+            <Link to="/">
+              <button style={{ float: "left" }}>Home</button>
+            </Link>
             <Link to="/trails">
               <button style={{ float: "left" }}>Trails</button>
             </Link>
@@ -79,112 +87,136 @@ function Navigation() {
             <Link to="/about">
               <button style={{ float: "left" }}>About</button>
             </Link>
-          </div>
-        </nav>
-        <Switch>
-          <Route path="/trails">
-            <TrailsIndex />
-          </Route>
-          <Route path="/reports">
-            <ReportsIndex />
-          </Route>
-          <Route path="/about">
-            <AboutIndex />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
-function formattedTrailsList() {
-
-}
-
-function TrailSelector() {
-  const trail_names = Object.keys(trails_list["wheaton regional"].trails);
-  return (
-    <div>
-      <label for="trails">Select a Trail</label>
-      <br />
-      <select name="trails" className="trails">
-        <option value="acorn woods">acorn woods</option>
-        <option value="arcola">arcola</option>
-        <option value="deer hollow">deer hollow</option>
-        <option value="oak ridge">oak ridge</option>
-        <option value="pine lake">pine lake</option>
-        <option value="tom's trail">tom's trail</option>
-        <option value="wheaton loop">tom's trail</option>
-      </select>
+          </nav>
+          <Switch>
+            <Route path="/trails">
+              <TrailsIndex />
+            </Route>
+            <Route path="/reports">
+              <ReportsIndex />
+            </Route>
+            <Route path="/about">
+              <AboutIndex />
+            </Route>
+          </Switch>
+        </div>
+      </HashRouter>
     </div>
   );
 }
 
-function TrailRating() {
-  return (
-    <div>
-      <input type="radio" name="rating" value="0"></input>
-      <label for="0">0</label>
-      <input type="radio" name="rating" value="1"></input>
-      <label for="1">1</label>
-      <input type="radio" name="rating" value="2"></input>
-      <label for="2">2</label>
-      <input type="radio" name="rating" value="3"></input>
-      <label for="3">3</label>
-      <input type="radio" name="rating" value="4"></input>
-      <label for="4">4</label>
-    </div>
-  );
-}
+function formattedTrailsList() {}
 
 function ReportsIndex() {
+  function TrailSelector() {
+    return (
+      <div>
+        <label for="trails">Select a Trail</label>
+        <br />
+        <select name="trails" className="trails">
+          <option value="acorn woods">Acorn Woods</option>
+          <option value="arcola">Arcola</option>
+          <option value="deer hollow">Deer Hollow</option>
+          <option value="oak ridge">Oak Ridge</option>
+          <option value="pine lake">Pine Lake</option>
+          <option value="tom's trail">Tom'S Trail</option>
+          <option value="wheaton loop">Wheaton Loop</option>
+        </select>
+      </div>
+    );
+  }
+
+  function TrailRating() {
+    return (
+      <div>
+        <input type="radio" name="rating" value="0"></input>
+        <label for="0">0</label>
+        <input type="radio" name="rating" value="1"></input>
+        <label for="1">1</label>
+        <input type="radio" name="rating" value="2"></input>
+        <label for="2">2</label>
+        <input type="radio" name="rating" value="3"></input>
+        <label for="3">3</label>
+        <input type="radio" name="rating" value="4"></input>
+        <label for="4">4</label>
+      </div>
+    );
+  }
+
   return (
-    <div style={{ width: "75%", paddingTop: "10px" }}>
-      <h1 style={{ float: "left", top: "30px" }}>Wheaton Trail Reports</h1>
-      <br />
-      <br />
-      <br />
-      <br />
-      <Navigation />
-      <main>
-        <div>
-          <details>
-            <summary>Submit a report</summary>
-            <p></p>
-            <TrailSelector />
-            <p></p>
-            <p></p>
-            <form>
-              Select Date
-              <input type="datetime-local" />
-              <p></p>
-              Trail Conditions
-              <textArea />
-              <p></p>
-              Trail Hazards
-              <textArea />
-              <p></p>
-              Needed Maintenance
-              <textArea />
-              <p></p>
-              Overall Trail Quality
-              <TrailRating />
-              <p></p>
-            </form>
-          </details>
-        </div>
-      </main>
+    <div>
+      <h1>Wheaton Trail Reports</h1>
+      <h2>Submit a Trail Report</h2>
+      You are welcome to submit a trail report for others to view. Click "Submit
+      a report" below to begin your report.
+      <details style={{ backgroundColor: "white" }}>
+        <summary>Submit a report</summary>
+        <p></p>
+        <TrailSelector />
+        <p></p>
+        <p></p>
+        <form>
+          Select Date
+          <input type="datetime-local" />
+          <p></p>
+          Trail Conditions
+          <textArea />
+          <p></p>
+          Trail Hazards
+          <textArea />
+          <p></p>
+          Needed Maintenance
+          <textArea />
+          <p></p>
+          Overall Trail Quality
+          <TrailRating />
+          <p></p>
+          <p></p>
+          <input type="submit"></input>
+        </form>
+      </details>
     </div>
   );
 }
 
 function TrailsIndex() {
   // <formattedTrailsList>
-  return <h1>Trails</h1>;
+  return (
+    <div>
+      <h1>Wheaton Trail Reports</h1>
+      <div>Trails</div>
+    </div>
+  );
 }
 
 function AboutIndex() {
-  return <h1>About</h1>;
+  return (
+    <div>
+      <h1>Wheaton Trail Reports</h1>
+      <div>
+        <h1>About</h1>
+        <strong>Address:  </strong>
+        {trails_list["wheaton regional"].address}
+        <br />
+        <strong>Phone:    </strong>
+        {trails_list["wheaton regional"].phone}
+        <br />
+        <strong>Web:      </strong>
+        <a href={trails_list["wheaton regional"].website}>
+          {trails_list["wheaton regional"].website}
+        </a>
+        <br />
+      </div>
+    </div>
+  );
 }
 
-ReactDOM.render(<ReportsIndex />, document.getElementById("root"));
+function Site() {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+}
+
+ReactDOM.render(<Site />, document.getElementById("root"));
